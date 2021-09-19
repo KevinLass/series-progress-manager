d619ac0d20dcdfdb197d106cf8465fd975fad898
Kevin Lass <>
2021-09-19 14:12:02 +0200

    Add files of current version

---
+++ FrmVlcViewer.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class FrmVlcViewer {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.BtnOpenFile = new System.Windows.Forms.Button();
+            this.LblFileName = new System.Windows.Forms.Label();
+            this.DgFiles = new System.Windows.Forms.DataGridView();
+            this.BtnVlc = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.BtnWatched = new System.Windows.Forms.Button();
+            this.BtnUnwatched = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnOpenFile
+            // 
+            this.BtnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.BtnOpenFile.Name = "BtnOpenFile";
+            this.BtnOpenFile.Size = new System.Drawing.Size(104, 38);
+            this.BtnOpenFile.TabIndex = 4;
+            this.BtnOpenFile.Text = "Open";
+            this.BtnOpenFile.UseVisualStyleBackColor = true;
+            this.BtnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // LblFileName
+            // 
+            this.LblFileName.AutoSize = true;
+            this.LblFileName.Location = new System.Drawing.Point(12, 76);
+            this.LblFileName.Name = "LblFileName";
+            this.LblFileName.Size = new System.Drawing.Size(14, 20);
+            this.LblFileName.TabIndex = 6;
+            this.LblFileName.Text = "-";
+            // 
+            // DgFiles
+            // 
+            this.DgFiles.AllowUserToAddRows = false;
+            this.DgFiles.AllowUserToDeleteRows = false;
+            this.DgFiles.AllowUserToResizeColumns = false;
+            this.DgFiles.AllowUserToResizeRows = false;
+            this.DgFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DgFiles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.DgFiles.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DgFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgFiles.DefaultCellStyle = dataGridViewCellStyle1;
+            this.DgFiles.Location = new System.Drawing.Point(12, 111);
+            this.DgFiles.MultiSelect = false;
+            this.DgFiles.Name = "DgFiles";
+            this.DgFiles.ReadOnly = true;
+            this.DgFiles.RowHeadersVisible = false;
+            this.DgFiles.RowHeadersWidth = 62;
+            this.DgFiles.RowTemplate.Height = 28;
+            this.DgFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DgFiles.Size = new System.Drawing.Size(1124, 951);
+            this.DgFiles.TabIndex = 7;
+            this.DgFiles.DoubleClick += new System.EventHandler(this.DgFiles_DoubleClick);
+            // 
+            // BtnVlc
+            // 
+            this.BtnVlc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnVlc.Location = new System.Drawing.Point(996, 12);
+            this.BtnVlc.Name = "BtnVlc";
+            this.BtnVlc.Size = new System.Drawing.Size(140, 38);
+            this.BtnVlc.TabIndex = 8;
+            this.BtnVlc.Text = "Start in VLC";
+            this.BtnVlc.UseVisualStyleBackColor = true;
+            this.BtnVlc.Click += new System.EventHandler(this.BtnVlc_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnClear.Location = new System.Drawing.Point(711, 12);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(121, 38);
+            this.BtnClear.TabIndex = 9;
+            this.BtnClear.Text = "Clear History";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(122, 12);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(112, 38);
+            this.BtnRefresh.TabIndex = 10;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // BtnWatched
+            // 
+            this.BtnWatched.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnWatched.Location = new System.Drawing.Point(512, 12);
+            this.BtnWatched.Name = "BtnWatched";
+            this.BtnWatched.Size = new System.Drawing.Size(88, 38);
+            this.BtnWatched.TabIndex = 11;
+            this.BtnWatched.Text = "Mark";
+            this.BtnWatched.UseVisualStyleBackColor = true;
+            this.BtnWatched.Click += new System.EventHandler(this.BtnWatched_Click);
+            // 
+            // BtnUnwatched
+            // 
+            this.BtnUnwatched.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnUnwatched.Location = new System.Drawing.Point(606, 12);
+            this.BtnUnwatched.Name = "BtnUnwatched";
+            this.BtnUnwatched.Size = new System.Drawing.Size(99, 38);
+            this.BtnUnwatched.TabIndex = 12;
+            this.BtnUnwatched.Text = "Unmark";
+            this.BtnUnwatched.UseVisualStyleBackColor = true;
+            this.BtnUnwatched.Click += new System.EventHandler(this.BtnUnwatched_Click);
+            // 
+            // FrmVlcViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 1074);
+            this.Controls.Add(this.BtnUnwatched);
+            this.Controls.Add(this.BtnWatched);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnVlc);
+            this.Controls.Add(this.DgFiles);
+            this.Controls.Add(this.LblFileName);
+            this.Controls.Add(this.BtnOpenFile);
+            this.Name = "FrmVlcViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Video Watcher";
+            ((System.ComponentModel.ISupportInitialize)(this.DgFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnOpenFile;
+        private System.Windows.Forms.Label LblFileName;
+        private System.Windows.Forms.DataGridView DgFiles;
+        private System.Windows.Forms.Button BtnVlc;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.Button BtnWatched;
+        private System.Windows.Forms.Button BtnUnwatched;
+    }
+}
---
+++ FrmVlcViewer.cs
@@ -0,0 +1,228 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    public enum enumColumns {
+        Watched = 1,
+        Filename = 2
+    }
+
+    public enum enumSettings {
+        [Description("VlcPath")]
+        VlcPath = 1,
+        [Description("CurrentlyWatchedFolder")]
+        CurrentlyWatchedFolder = 2
+    }
+
+    public partial class FrmVlcViewer : Form {
+
+        private Dictionary<string, DateTime> _videosWatched;
+        private readonly Dictionary<string, string> _settings;
+
+        // Execution path
+        private readonly string _watchedVideosPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "WatchedVideos.json");
+        private readonly string _settingsPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "Settings.json");
+
+        public FrmVlcViewer() {
+            InitializeComponent();
+
+            if (File.Exists(_watchedVideosPath)) {
+                using (StreamReader file = File.OpenText(_watchedVideosPath)) {
+                    JsonSerializer serializer = new JsonSerializer();
+                    _videosWatched = (Dictionary<string, DateTime>)serializer.Deserialize(file, typeof(Dictionary<string, DateTime>));
+                }
+            } else {
+                _videosWatched = new Dictionary<string, DateTime>();
+            }
+
+            if (File.Exists(_settingsPath)) {
+                using (StreamReader file = File.OpenText(_settingsPath)) {
+                    JsonSerializer serializer = new JsonSerializer();
+                    _settings = (Dictionary<string, string>)serializer.Deserialize(file, typeof(Dictionary<string, string>));
+                }
+            } else {
+                _settings = new Dictionary<string, string>() {
+                    {  "CurrentlyWatchedFolder", string.Empty},
+                    {  "VlcPath", @"C:\Program Files\VideoLAN\VLC\vlc.exe"},
+                };
+            }
+
+            if (Directory.Exists(_settings["CurrentlyWatchedFolder"])) {
+                LblFileName.Text = _settings["CurrentlyWatchedFolder"];
+                LoadFileNames(LblFileName.Text);
+            }
+        }
+
+        #region File Operations
+        private void LoadFileNames(string path) {
+            String[] files = Directory.GetFiles(path);
+
+            DataTable table = new DataTable();
+
+            table.Columns.Add("Watched");
+
+            table.Columns.Add("File Name");
+            foreach(var file in files) {
+                FileInfo info = new FileInfo(file);
+                if (info.Extension == ".mkv" || info.Extension == ".vlc") {
+                    DataRow dr = table.NewRow();
+                    dr["File Name"] = info.Name;
+                    table.Rows.Add(dr);
+                }
+            }
+
+            RefreshWatched(table);
+
+            DgFiles.DataSource = table;
+        }
+
+        private void UpdateDictionary(string videoName) {
+            if (!string.IsNullOrWhiteSpace(videoName)) {
+                if (_videosWatched.ContainsKey(videoName)) {
+                    _videosWatched[videoName] = DateTime.Now;
+                } else {
+                    _videosWatched.Add(videoName, DateTime.Now);
+                }
+            }
+            SaveDictAsJson();
+            RefreshWatched((DataTable)DgFiles.DataSource);
+        }
+
+
+        private void RemoveFromDictionary(string videoName) {
+            if (_videosWatched.ContainsKey(videoName)) {
+                _videosWatched.Remove(videoName);
+                SaveDictAsJson();
+                RefreshWatched((DataTable)DgFiles.DataSource);
+            }
+        }
+        private void SaveDictAsJson() {
+            var json = JsonConvert.SerializeObject(_videosWatched);
+            File.WriteAllText(_watchedVideosPath, json);
+        }
+
+        #endregion
+
+        #region Table Operations
+
+        private void RefreshWatched(DataTable table) {
+            foreach (DataRow row in table.Rows) {
+                string rowData = row["File Name"].ToString();
+                if (_videosWatched.ContainsKey(rowData)) {
+                    row["Watched"] = _videosWatched[row["File Name"].ToString()].ToString("g");
+                } else {
+                    row["Watched"] = string.Empty;
+                }
+            }
+        }
+
+        private string GetSelectedName() {
+            if (DgFiles.SelectedRows.Count == 0) {
+                ShowError("No Video selected.");
+                return null;
+            }
+
+            var file = DgFiles.SelectedRows[0];
+            return (string)file.Cells["File Name"].Value;
+        }
+
+        #endregion
+
+        #region Event Handler
+
+        private void BtnVlc_Click(object sender, EventArgs e) {
+            try {
+
+                string videoName = GetSelectedName();
+                string videoPath = $"{LblFileName.Text}\\{videoName}";
+
+                if (File.Exists(videoPath)) {
+
+                    if (!File.Exists(_settings["VlcPath"])) {
+                        ShowError("vlc.exe not found. Make sure that the path in the Settings.json is correct.");
+                        return;
+                    }
+
+                    Process p = new Process();
+                    p.StartInfo.FileName = _settings["VlcPath"];
+                    p.StartInfo.Arguments = "\"" + videoPath + "\"";
+                    p.Start();
+                } else {
+                    ShowError("Video was not found.");
+                    return;
+                }
+
+                UpdateDictionary(videoName);
+
+            } catch (Exception ex) {
+                ShowError(ex.Message);
+            }
+        }
+
+        private void BtnOpenFile_Click(object sender, EventArgs e) {
+            var dialog = new FolderBrowserDialog() {
+                ShowNewFolderButton = false,
+                SelectedPath = Directory.Exists(_settings["CurrentlyWatchedFolder"]) ? _settings["CurrentlyWatchedFolder"] : null
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                LblFileName.Text = dialog.SelectedPath;
+
+                _settings["CurrentlyWatchedFolder"] = dialog.SelectedPath;
+                var json = JsonConvert.SerializeObject(_settings);
+                File.WriteAllText(_settingsPath, json);
+                
+                LoadFileNames(LblFileName.Text);
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e) {
+            if(MessageBox.Show("Do you really want to delete all the history?", "Really?",
+               MessageBoxButtons.YesNo) == DialogResult.Yes) {
+
+                _videosWatched = new Dictionary<string, DateTime>();
+                UpdateDictionary(null);
+            }
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e) {
+            LoadFileNames(LblFileName.Text);
+        }
+
+        private void BtnWatched_Click(object sender, EventArgs e) {
+            string name = GetSelectedName();
+            if (name != null) {
+                UpdateDictionary(name);
+            }
+        }
+
+        private void BtnUnwatched_Click(object sender, EventArgs e) {
+            string name = GetSelectedName();
+            if (name != null) {
+                RemoveFromDictionary(name);
+            }
+        }
+
+        private void DgFiles_DoubleClick(object sender, EventArgs e) {
+            BtnVlc_Click(sender, e);
+        }
+
+        #endregion
+
+
+        private void ShowError(string message) {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK);
+        }
+    }
+}
